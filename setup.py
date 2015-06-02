75f841c3b46dce0bdfac2c5fb623f71ae0c02ff5
Colin Raffel <craffel@gmail.com>
2015-06-02 14:29:00 -0400

    Fix setup.py wording

--- setup.py
+++ setup.py
@@ -3,27 +3,27 @@ from setuptools import setup
 setup(
     name='pretty_midi',
     version='0.0.1',
-    description='A class for handling MIDI data in a convenient way.',
+    description='Functions and classes for handling MIDI data conveniently.',
     author='Colin Raffel',
     author_email='craffel@gmail.com',
     url='https://github.com/craffel/pretty_midi',
     packages=['pretty_midi'],
     package_data={'': ['TimGM6mb.sf2']},
     long_description="""\
-    A class which makes handling MIDI data easy in Python.  Provides methods
-    for extracting and modifying the useful parts of MIDI files.
+    Functions and classes which make handling MIDI data easy in Python.
+    Provides methods for parsing, modifying, and analyzing MIDI files.
     """,
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "Topic :: Multimedia :: Sound/Audio :: Analysis",
+        "Topic :: Multimedia :: Sound/Audio :: MIDI",
     ],
     keywords='audio music midi mir',
     license='MIT',
     install_requires=[
         'numpy >= 1.7.0',
-	'midi'
+        'midi'
     ],
 )
