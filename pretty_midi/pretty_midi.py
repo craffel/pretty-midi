6933988ca83c73accda074d579a320b5e357c7c1
Rafael Valle <jrafaelvalle@gmail.com>
2015-08-04 14:31:48 -0400

    formatting

--- pretty_midi/pretty_midi.py
+++ pretty_midi/pretty_midi.py
@@ -113,7 +113,7 @@ class PrettyMIDI(object):
             self.instruments = []
 
     def _load_tempo_changes(self, midi_data):
-        """Populates self.__tick_scales with tuples of (tick, tick_scale),
+        """Populates self.__tick_scales with tuples of (tick, tick_scale)
 
         Parameters
         ----------
