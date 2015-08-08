c9e19b9b924a1339ddc1e4543ae6cb441e44fb1a
Colin Raffel <craffel@gmail.com>
2015-08-08 01:39:58 -0400

    Remove cruft from __init__.py

--- pretty_midi/__init__.py
+++ pretty_midi/__init__.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-"""Utility functions for handling MIDI data in an easy to read/manipulate
-format"""
-
-# Import all submodules (for each task)
 from pretty_midi import *
 
 __version__ = '0.1'
